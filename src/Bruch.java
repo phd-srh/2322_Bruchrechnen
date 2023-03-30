fd107ae3d31657d38d3e4752a2320a7bb0ccb5f7
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-03-30 13:43:32 +0200

    Erster Konstruktor, erste Methode add und Hilfsmethoden toString und kürze implementiert.

--- src/Bruch.java
+++ src/Bruch.java
@@ -1,2 +1,33 @@
+// immutable
 public class Bruch {
+    private final long zähler;
+    private final long nenner;
+
+    public Bruch(long zähler, long nenner) {
+        this.zähler = zähler;
+        this.nenner = nenner;
+    }
+
+    public Bruch add(Bruch b) {
+        long neuerNenner = this.nenner * b.nenner;
+        long neuerZähler = this.zähler * b.nenner + b.zähler * this.nenner;
+        return new Bruch(neuerZähler, neuerNenner).kürze();
+    }
+
+    public Bruch kürze() {
+        long teiler = ggT(zähler, nenner);
+        return new Bruch( zähler/teiler, nenner/teiler );
+    }
+
+    private long ggT(long a, long b) {
+        while (a != b) {
+            if (a > b) a = a - b;
+            else       b = b - a;
+        }
+        return a;
+    }
+
+    public String toString() {
+        return zähler + "/" + nenner;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         Bruch b2 = new Bruch(3, 4);
         Bruch b3 = b1.add(b2);
         System.out.println( b3.toString() );
+
+        Bruch b4 = new Bruch(1, 3);
+        Bruch b5 = new Bruch(2, 5);
+        Bruch b6 = b4.add(b5);
+        System.out.println( b6.toString() );
     }
 }
